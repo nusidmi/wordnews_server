d0c5f6ac0dcba04a3d5b870f9ecf82ecec275830
Tao Chen <chentaokite@gmail.com>
2016-09-02 11:41:03 +0000

    fix ruby calls ims

--- lib/utilities/ims_translator.rb
+++ lib/utilities/ims_translator.rb
@@ -7,18 +7,17 @@ module Utilities::ImsTranslator
   def self.translate(word, word_position, sentence)
     sentence = sentence[0..(word_position[0]-1)] + @@IMS_FLAG + sentence[word_position[0]..-1]
     params = {"sentence": sentence, "word": word}
-    #params = {:sentence=>sentence, :word=>word}
-    response = HTTParty.post(IMS_HOST+'/translate_word', :query=>params)
+    response = HTTParty.post(IMS_HOST+'/translate_word', 
+                            :body=>params.to_json, 
+				                    :headers => {'Content-Type' => 'application/json'})
+
     
     if response.code!=200 or response.body==''
       puts 'Error in calling IMS for ' + word
       return
     end
     
-    #result = JSON.parse(response.body)
-    puts '-----ims ' + response.body
-    puts word
-    puts sentence
+    result = JSON.parse(response.body)
     if result['msg']=='OK'
       return result['translation']
     end
